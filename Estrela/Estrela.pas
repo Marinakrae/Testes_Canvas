1d6ed2f0d12523a83140e935b7007abda4f0e617
Marina Krause Eskelsen <marina.eskelsen@acad.ufsm.br>
2023-01-30 16:55:50 -0300

    Começo do desenho de uma estrela

---
+++ Estrela/Estrela.pas
@@ -0,0 +1,94 @@
+unit Estrela;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, ExtCtrls, StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Desenho: TImage;
+    Button1: TButton;
+    procedure FormPaint(Sender: TObject);
+    procedure Button1Click(Sender: TObject);
+    function DefinePontoX(xCentro, yCentro, raio, angulo: Double): Integer;
+    function DefinePontoY(xCentro, yCentro, raio, angulo: Double):Integer;
+  private
+    { Private declarations }
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+uses Math;
+
+{$R *.dfm}
+
+//procedure TForm1.FormPaint(Sender: TObject);
+//var
+//    i, x, y, xx, yy: Integer;
+//    p: array[0..9] of TPoint;
+//    t, r, sv, sw, sx, sy, s: Real;
+//    Rect: TRect;
+//    FRegion: HRGN;
+//begin
+//{Desenhando a estrela}
+//  With Desenho do
+//  begin
+//    Rect := ClientRect;
+//    InflateRect(Rect, -2, -2); //decreases rectangle size by 2//
+//    sx := (Rect.Right-Rect.Left)*0.48;
+//    sy := (Rect.Bottom-Rect.Top)*0.5;
+//    if sx > sy then
+//      sx := sy
+//    else
+//      sy := sx;
+//    sv := (Rect.Left + Rect.Right) / 2;
+//    sw := (Rect.Top + Rect.Bottom * 1.2) / 2.2;
+//    for i := 0 to 10 do
+//    begin
+//      if ((i and 1) <> 0) then
+//        r := 1
+//      else
+//        r := 0.384;
+//      t := i * 2 * (PI/10);
+//      P[i].x := Trunc(sv+sx*r*sin(t));
+//      P[i].Y := Trunc(sw+sy*r*cos(t));
+//    end;
+//    FRegion := CreatePolygonRgn(P, 10, WINDING);
+//  end
+//end;
+
+function TForm1.DefinePontoX(xCentro, yCentro, raio, angulo: Double): Integer;
+begin
+  result := Round(xCentro+raio/Secant(angulo));
+end;
+
+function TForm1.DefinePontoY(xCentro, yCentro, raio, angulo: Double) : Integer;
+begin
+  result := Round(yCentro+raio/Cosecant(angulo));
+end;
+
+procedure TForm1.FormPaint(Sender: TObject);
+begin
+{Desenhando a estrela}
+  with Desenho do
+  begin
+    canvas.moveto(DefinePontoX(200.0, 200.0, 100.0, 0.0), DefinePontoY(200.0, 200.0, 100.0, 0.0));
+  end;
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  with Desenho do
+  begin
+    FormPaint(Desenho);
+  end;
+end;
+
+end.
